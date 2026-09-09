3bfea0260d66676fe1f668fd832dcdf22ef82485
abbadasarishainy <abbadasarishainy@gmail.com>
2026-09-09 19:22:37 +0530

    Create character.py

---
+++ character.py
@@ -0,0 +1,7 @@
+N=input()
+if not s;
+print("Empty string")else;
+freq-(c:s.count(c)for c in s)
+counts=sorted(set(freg. values()), reverse=True)if len(counts)<2:
+print("No second Character")else;
+print([c for c in s if freq[c]-ecounts[1]][0])
